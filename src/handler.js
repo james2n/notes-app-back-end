42a9aa5a8ca538fd862dfcbab6aaff4b8790f2dc
James <jameskristiand@gmail.com>
2021-06-07 23:51:51 +0900

    Membuat fungsi menambah catatan pada handler.js

---
+++ src/handler.js
@@ -0,0 +1,48 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+// membuat fungsi handler
+const addNoteHandler = (request, h) => {
+  // properti yang di dpaat dari client
+  const { title, tags, body } = request.payload;
+
+  // Properti yang ditangani oleh sistem/server
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createdAt,
+    updatedAt,
+  };
+  notes.push(newNote);
+
+  // Cek apakah newNote sudah masuk ke dalam array notes
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  // Gunakan isSuccess untuk menentukan response yang diberikan server, true dan false
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = { addNoteHandler };
+// eksport fungsi handler menggunakan object literals, agar mudah mengeksport lebih dari 1 nilai.
